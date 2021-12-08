a532acce43e0fc8b0db1f330f44a50adc6ebc9a8
Mi <twinsminsk@yandex.ru>
2021-12-08 12:08:30 +0300

    Исправлены ошибки при вводе и написаны f'строки'

--- cowandbulls.py
+++ cowandbulls.py
@@ -1,3 +1,4 @@
+import string
 from random import choice
 
 num = '0123456789'
@@ -7,17 +8,25 @@ for n in range(3):
     num = ''.join(num.split(x[n]))
     x += choice(num)
 print('Загаданное число - ', x)
+
 while True:
     y = input('Введите 4-х значное число: ')
-    bulls = 0
-    cows = 0
-    step += 1
-    for n in range(4):
-        if x[n] == y[n]:
-            bulls += 1
-        elif y[n] in x:
-            cows += 1
-    print(y + ' содержит ' + str(bulls) + ' быка и ' + str(cows) + ' коров')
-    if bulls == 4:  # Если число угадано
-        print('Вы победили за '+ str(step) + ' ходов')  # Победа
-        break
+    if len(y) == 4 and y.isdigit():
+        bulls = 0
+        cows = 0
+        step += 1
+        for n in range(4):
+            if x[n] == y[n]:
+                bulls += 1
+            elif y[n] in x:
+                cows += 1
+        print(f'{y} содержит {bulls} быка и {cows} коров')
+        if bulls == 4:  # Если число угадано
+            print(f'Вы победили за {step} ходов')  # Победа
+            break
+    else:
+        print("Число введено неверно, попробуйте еще раз...")
+        continue
+
+
+
